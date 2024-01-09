dd089935b744d5070fc79b578fa1c2ead64e0238
nicholassouto <nssouto@hotmail.com>
2024-01-09 15:08:01 -0300

    changes in company auth

--- src/main/java/br/com/nssouto/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/nssouto/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -14,13 +14,13 @@ import br.com.nssouto.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.nssouto.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/company")
 public class AuthCompanyController {
 
     @Autowired
     private AuthCompanyUseCase authCompanyUseCase;
 
-    @PostMapping("/company")
+    @PostMapping("/auth")
     public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         try {
             var result = this.authCompanyUseCase.execute(authCompanyDTO);
