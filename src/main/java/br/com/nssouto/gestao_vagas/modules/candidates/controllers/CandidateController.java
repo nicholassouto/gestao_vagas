e46159c1401bfc37ea4c09e9bd9d965a8e63fcbc
nicholassouto <nssouto@hotmail.com>
2024-01-10 18:14:43 -0300

    add swagger and change to adjust to it

--- src/main/java/br/com/nssouto/gestao_vagas/modules/candidates/controllers/CandidateController.java
+++ src/main/java/br/com/nssouto/gestao_vagas/modules/candidates/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.nssouto.gestao_vagas.modules.candidates.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nssouto.gestao_vagas.modules.candidates.CandidateEntity;
 import br.com.nssouto.gestao_vagas.modules.candidates.useCases.CreateCandidateUseCase;
+import br.com.nssouto.gestao_vagas.modules.candidates.useCases.ListAllJobsByFilterUseCase;
 import br.com.nssouto.gestao_vagas.modules.candidates.useCases.ProfileCandidateUseCase;
+import br.com.nssouto.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -26,6 +38,9 @@ public class CandidateController {
 
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -49,5 +64,18 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função listará todas as vagas disponíveis, baseadas no filtro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))) })
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
